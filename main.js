30eff36351f2791eb2305a52e9a31173b826579c
cs-sd1 <joaoenocsacassole@gmail.com>
2025-12-29 11:54:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+console.log("Alfredo Services ativo");
---
+++ supabase.js
@@ -0,0 +1,8 @@
+<script type="module">
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+window.supabase = createClient(
+  "https://SEU_PROJETO.supabase.co",
+  "PUBLIC_ANON_KEY"
+);
+</script>
